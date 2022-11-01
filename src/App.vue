c4d4f63a849da2ba737188763fe02a7408ca7210
unknown <Teleinformatica II@FDBAPRCISFSD378.FormacionSena.RED>
2022-11-01 15:34:51 -0500

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+<men>
+  
+</men>
+  </v-app>
+     
+    
+  
+</template>
+
+<script>
+import men from './components/MenuLogin.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    men,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
